d4210dcde5fc391a96504a662a16548d05f1eb7b
dbmarch <dbmarch@gmail.com>
2018-08-06 19:45:50 -0500

    Add client Id

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import {Security, ImplicitCallback} from '@okta/okta-react';
+import { Security, ImplicitCallback } from "@okta/okta-react";
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import MessageList from './components/MessageList';
+import MessageList from "./components/MessageList";
 
 import logo from "./logo.svg";
 import "./App.css";
-import Home from './routes/Home';
+import Home from "./routes/Home";
 
-const yourOktaDomain = "dev-657184.oktapreview.com"
+const yourOktaDomain = "dev-657184.oktapreview.com";
+const clientId = "0oafvmim6lHM5UqLH0h7";
 
 const config = {
   issuer: `https://${yourOktaDomain}/oauth2/default`,
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '{clientId}'
-}
+  redirect_uri: window.location.origin + "/implicit/callback",
+  client_id: "{clientId}"
+};
 
 class App extends Component {
   state = {
@@ -26,10 +27,11 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         this.setState({ message: response.message });
-      }).catch((e) => {
-        return console.log (e);
       })
-  ;}
+      .catch(e => {
+        return console.log(e);
+      });
+  }
 
   render() {
     return (
@@ -42,15 +44,16 @@ class App extends Component {
           The server says.... <b>{this.state.message}</b>
         </p>
         <Router>
-        <Security issuer={config.issuer}
-                  client_id={config.client_id}
-                  redirect_uri={config.redirect_uri}
-        >
-          <Route path='/' exact={true} component={Home}/>
-          <Route path='/implicit/callback' component={ImplicitCallback}/>
-          <MessageList />
-        </Security>
-      </Router>
+          <Security
+            issuer={config.issuer}
+            client_id={config.client_id}
+            redirect_uri={config.redirect_uri}
+          >
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+            <MessageList />
+          </Security>
+        </Router>
       </div>
     );
   }
